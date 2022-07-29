517ba74c8490708b94aa525e5075419cc46f6627
sujinnyan <ansujinny@gmail.com>
2022-07-29 11:08:33 -0400

    edited file

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import emailjs from 'emailjs-com'
 
 const Contact = () => {
